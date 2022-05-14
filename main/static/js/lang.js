be681865d6ea54517e1b738df33e2ae526c8d647
Solovev26 <solovev26.ru@gmail.com>
2022-05-14 19:40:32 +0300

    Added localization

---
+++ main/static/js/lang.js
@@ -0,0 +1,50 @@
+const langArr = {
+    "hello" :  {
+        "ru": "Привет",
+        "en": "Hello",
+    },
+    "info": {
+        "ru": "Информация",
+        "en": "Information",
+    },
+    "works": {
+        "ru": "Работы",
+        "en": "Works",
+    },
+    "links": {
+        "ru": "Ссылки",
+        "en": "Links",
+    },
+    "phones": {
+        "ru": "Телефоны для связи",
+        "en": "Phones for communication",
+    },
+    "emails": {
+        "ru": "Почты для связи",
+        "en": "Mail for communication",
+    },
+    "feedbackCall": {
+        "ru": "Связь со мной",
+        "en": "Contact me",
+    },
+    "name" :  {
+        "ru": "Имя",
+        "en": "Name",
+    },
+    "email" :  {
+        "ru": "Почта",
+        "en": "Mail",
+    },
+    "message" :  {
+        "ru": "Сообщение",
+        "en": "Message",
+    },
+    "sendButton" :  {
+        "ru": "Отправить сообщение",
+        "en": "Send message",
+    },
+    "checkInfo" :  {
+        "ru": "Посмотреть информацию",
+        "en": "View information",
+    },
+}
