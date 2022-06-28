6e53752dd1109d2e7072ed5ef3915c0a0d549dcc
Solovev26 <solovev26.ru@gmail.com>
2022-06-28 18:33:00 +0300

    First steps in Django Added: migrations, forms, views Docker added, container located in docker hub https://hub.docker.com/repository/docker/solovev26/python-django

--- main/views.py
+++ main/views.py
@@ -7,7 +7,6 @@ from .forms import UserDataForm, MsgForm
 # Create your views here.
 
 def index(request):
-  
     if request.method == 'POST':
         email = request.POST.get('email', False)
         password = request.POST.get('password', False)
@@ -16,75 +15,73 @@ def index(request):
         flag = False
         users = UserData.objects.all()
         for user in users:
-          if(user.e_mail == email and user.password == password):
-            flag = True
-            id = user.id
-            break
+            if (user.e_mail == email and user.password == password):
+                flag = True
+                id = user.id
+                break
 
-          
         if flag:
             request.session['id'] = id
             return redirect('/user')
-            #return redirect('/user', {'email': email, 'password':password})
+            # return redirect('/user', {'email': email, 'password':password})
         else:
             return redirect("/index")
-      
-    return render(request,'main/index.html')
+
+    return render(request, 'main/index.html')
 
 
 def info(request):
-    return render(request,'main/info.html')
+    return render(request, 'main/info.html')
 
 
 def works(request):
-    return render(request,'main/works.html')
+    return render(request, 'main/works.html')
 
 
 def links(request):
     form = MsgForm()
     if request.method == 'POST':
-      email = request.POST.get('email', False)
-      name = request.POST.get('name', False)
-      message = request.POST.get('message', False)
-      form=Message()
-      form.e_mail = email
-      form.f_name = name
-      form.msg = message
-      form.save()
-      return redirect('/links')
+        email = request.POST.get('email', False)
+        name = request.POST.get('name', False)
+        message = request.POST.get('message', False)
+        form = Message()
+        form.e_mail = email
+        form.f_name = name
+        form.msg = message
+        form.save()
+        return redirect('/links')
     context = {
-      'form': form
+        'form': form
     }
-    return render(request,'main/links.html', context)
+    return render(request, 'main/links.html', context)
 
 
 def registration(request):
     error = ''
     if request.method == 'POST':
-      form = UserDataForm(request.POST)
-      if form.is_valid():
-        form.save()
-        return redirect('/index')
-      else:
-        error = 'invalid input'
-        
-  
+        form = UserDataForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/index')
+        else:
+            error = 'invalid input'
+
     form = UserDataForm()
     context = {
-      'form': form,
-      'error': error
+        'form': form,
+        'error': error
     }
-    return render(request,'main/registration.html', context)
+    return render(request, 'main/registration.html', context)
 
 
 def user(request):
     id = request.session['id']
     flag = False
-    true_user=[]
+    true_user = []
     users = UserData.objects.all()
     for user in users:
         if user.id == id:
-          true_user = user
-          break
-    print(true_user.l_name,true_user.f_name)
-    return render(request,'main/user.html', {'true_user': true_user})
+            true_user = user
+            break
+    print(true_user.l_name, true_user.f_name)
+    return render(request, 'main/user.html', {'true_user': true_user})
